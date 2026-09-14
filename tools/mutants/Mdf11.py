88e9cfefbc9dcc5d40ccefc3e4c0b2b1738f50f0
Ayla Croft <aylacroft@proton.me>
2026-09-14 15:33:23 -0400

    The unreachable invalid_graph branch goes: Graph.check/1 has already passed
    Signed-off-by: Ayla Croft <aylacroft@proton.me>

--- tools/mutants/Mdf11.py
+++ tools/mutants/Mdf11.py
@@ -10,9 +10,8 @@ import sys
 p = sys.argv[1]
 s = open(p).read()
 
-old = "      {:error, {:uncanonical, {:invalid_graph, reason}}} -> {:error, reason}\n      other -> other"
-new = "      {:error, {:uncanonical, {:invalid_graph, reason}}} -> {:error, reason}\n      _other -> :ok"
-
+old = "  defp canonical(graph), do: Canonical.check(graph)"
+new = "  defp canonical(graph), do: (if is_map(graph), do: :ok, else: Canonical.check(graph))"
 if s.count(old) != 1:
     sys.exit("Mdf11: anchor found %d times" % s.count(old))
 
